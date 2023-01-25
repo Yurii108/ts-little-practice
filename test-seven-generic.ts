c63a043590ee1960705b4b2093db9153e8dedd9d
Yurii <paraska108@gmail.com>
2023-01-25 11:57:13 +0200

    add new test with generic, finish one-part

--- test-seven-generic.ts
+++ test-seven-generic.ts
@@ -1,24 +1,35 @@
 // Создать Generic-интерфейс PlayerData, который подходил бы для создания таких объектов:
 
-const player1 = {
-	game: "CS:GO",
-	hours: 300,
-	server: "basic",
+interface PlayerOfHours {
+  total: number;
+  inMenu: number;
+}
+
+interface PlayerData<H extends PlayerOfHours | number | string> {
+  game: string | number;
+  hours: H;
+  server: string;
+}
+
+const player1: PlayerData<number> = {
+  game: "CS:GO",
+  hours: 300,
+  server: "basic",
 };
 
-const player2 = {
-	game: 2048,
-	hours: "300 h.",
-	server: "arcade",
+const player2: PlayerData<string> = {
+  game: 2048,
+  hours: "300 h.",
+  server: "arcade",
 };
 
-const player3 = {
-	game: "Chess",
-	hours: {
-		total: 500,
-		inMenu: 50,
-	},
-	server: "chess",
+const player3: PlayerData<PlayerOfHours> = {
+  game: "Chess",
+  hours: {
+    total: 500,
+    inMenu: 50,
+  },
+  server: "chess",
 };
 
 // Массив данных с фигурами содержит объекты, у каждого из которых обязательно есть свойство name
@@ -31,46 +42,46 @@ const player3 = {
 // { squares: 3, circles: 2, triangles: 2, others: 1 }
 
 interface AmountOfFigures {
-	squares: number;
-	circles: number;
-	triangles: number;
-	others: number;
+  squares: number;
+  circles: number;
+  triangles: number;
+  others: number;
 }
 
 function calculateAmountOfFigures(figure): AmountOfFigures {}
 
 const data = [
-	{
-		name: "rect",
-		data: { a: 5, b: 10 },
-	},
-	{
-		name: "rect",
-		data: { a: 6, b: 11 },
-	},
-	{
-		name: "triangle",
-		data: { a: 5, b: 10, c: 14 },
-	},
-	{
-		name: "line",
-		data: { l: 15 },
-	},
-	{
-		name: "circle",
-		data: { r: 10 },
-	},
-	{
-		name: "circle",
-		data: { r: 5 },
-	},
-	{
-		name: "rect",
-		data: { a: 15, b: 7 },
-	},
-	{
-		name: "triangle",
-	},
+  {
+    name: "rect",
+    data: { a: 5, b: 10 },
+  },
+  {
+    name: "rect",
+    data: { a: 6, b: 11 },
+  },
+  {
+    name: "triangle",
+    data: { a: 5, b: 10, c: 14 },
+  },
+  {
+    name: "line",
+    data: { l: 15 },
+  },
+  {
+    name: "circle",
+    data: { r: 10 },
+  },
+  {
+    name: "circle",
+    data: { r: 5 },
+  },
+  {
+    name: "rect",
+    data: { a: 15, b: 7 },
+  },
+  {
+    name: "triangle",
+  },
 ];
 
 console.log(calculateAmountOfFigures(data));

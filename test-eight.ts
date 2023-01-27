88275d0dabce0610f7df71a23dbad7e0331b03b8
Yurii <paraska108@gmail.com>
2023-01-27 13:06:59 +0200

    add t-eight

--- test-eight.ts
+++ test-eight.ts
@@ -1,55 +1,66 @@
 interface IPhone {
-	company: string;
-	number: number;
+  company: string;
+  number: number;
 }
 
 // IMobilePhone должен наследоваться от IPhone,
 // тип свойства companyPartner зависит от свойства company
 
-interface IMobilePhone {
-	size: string;
-	companyPartner: "same type as company in Phone";
-	manufactured: Date;
+interface IMobilePhone extends IPhone {
+  size: string;
+  companyPartner: IPhone["company"];
+  manufactured: Date;
 }
 
 // Типизировать объект phones
 
-const phones = [
-	{
-		company: "Nokia",
-		number: 1285637,
-		size: "5.5",
-		companyPartner: "MobileNokia",
-		manufactured: new Date("2022-09-01"),
-	},
-	{
-		company: "Samsung",
-		number: 4356637,
-		size: "5.0",
-		companyPartner: "SamMobile",
-		manufactured: new Date("2021-11-05"),
-	},
-	{
-		company: "Apple",
-		number: 4552833,
-		size: "5.7",
-		companyPartner: "no data",
-		manufactured: new Date("2022-05-24T12:00:00"),
-	},
+const phones: IMobilePhone[] = [
+  {
+    company: "Nokia",
+    number: 1285637,
+    size: "5.5",
+    companyPartner: "MobileNokia",
+    manufactured: new Date("2022-09-01"),
+  },
+  {
+    company: "Samsung",
+    number: 4356637,
+    size: "5.0",
+    companyPartner: "SamMobile",
+    manufactured: new Date("2021-11-05"),
+  },
+  {
+    company: "Apple",
+    number: 4552833,
+    size: "5.7",
+    companyPartner: "no data",
+    manufactured: new Date("2022-05-24T12:00:00"),
+  },
 ];
 
 interface IPhonesManufacturedAfterDate extends IMobilePhone {
-	initialDate: string;
+  initialDate: string;
 }
 
 // Функция должна отфильтровать массив данных и вернуть новый массив
 // с телефонами, выпущенными после даты в третьем аргументе
+//IPhonesManufacturedAfterDate[]
 
-function filterPhonesByDate(
-	phones: [],
-	key: string,
-	initial: string
-): IPhonesManufacturedAfterDate[] {}
+function filterPhonesByDate<T, K extends keyof T>(
+  phones: T[],
+  key: K,
+  initial: string
+) {
+  const data = [];
+
+  for (let k of phones) {
+    if (k[key] > new Date(initial)) {
+      data.push(k);
+    }
+  }
+
+  return data;
+}
 
 // Второй аргумент при вызове функции должен быть связан с первым,
 // а значит мы получим подсказки - свойства этого объекта

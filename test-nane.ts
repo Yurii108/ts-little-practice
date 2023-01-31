8cdb6ddfbd398183ecdc7d6ace39ed6a0ed6d04a
Yurii <paraska108@gmail.com>
2023-01-31 13:30:29 +0200

    finished t-nane

--- test-nane.ts
+++ test-nane.ts
@@ -6,70 +6,102 @@
 // Простыми словами: при добавлении свойства в целевой объект они должны быть
 // автоматически добавлены в зависимые (сразу подсказка от TS)
 
-const fitnessClubCenter = {
-	clubName: "Fitness club Center",
-	location: "central ave. 45, 5th floor",
-	classes: [
-		{
-			name: "yoga",
-			startsAt: "8:00 AM",
-			duration: 60,
-		},
-		{
-			name: "trx",
-			startsAt: "11:00 AM",
-			duration: 45,
-		},
-		{
-			name: "swimming",
-			startsAt: "3:00 PM",
-			duration: 70,
-		},
-	],
-	futureClasses: [
-		{
-			name: "boxing",
-			willStartsAt: "6:00 PM",
-			duration: 40,
-		},
-		{
-			name: "breath training",
-			willStartsAt: "8:00 PM",
-			duration: 30,
-		},
-	],
-	currClients: [
-		{
-			name: "John Smith",
-			age: "-",
-			gender: "male",
-			timeLeft: "1 month",
-		},
-		{
-			name: "Alise Smith",
-			age: 35,
-			gender: "female",
-			timeLeft: "3 month",
-		},
-		{
-			name: "Ann Sonne",
-			age: 24,
-			gender: "female",
-			timeLeft: "5 month",
-		},
-	],
-	exClients: [
-		{
-			name: "Tom Smooth",
-			age: 50,
-			gender: "male",
-			makeCallFor: new Date("2023-08-12"),
-		},
-	],
-	futureClients: [
-		{
-			name: "Maria",
-			makeCallFor: new Date("2023-07-10"),
-		},
-	],
+interface IFitnessClubCenter {
+  clubName: string;
+  location: string;
+  classes: IClasses[];
+  futureClasses: IFutureClasses[];
+  currClients: ICurrClients[];
+  exClients: IExClients[];
+  futureClients: IFutureClients[];
+}
+
+interface IClasses {
+  name: string;
+  startsAt: string;
+  duration: number;
+}
+
+interface IFutureClasses extends Omit<IClasses, "startsAt"> {
+  willStartsAt: string;
+}
+
+interface IAllClients {
+  name: string;
+  age: number | string;
+  gender: "male" | "female";
+  timeLeft: string;
+  makeCallFor: Date;
+}
+
+type ICurrClients = Omit<IAllClients, "makeCallFor">;
+type IExClients = Omit<IAllClients, "timeLeft">;
+type IFutureClients = Pick<IAllClients, "name" | "makeCallFor">;
+
+const fitnessClubCenter: IFitnessClubCenter = {
+  clubName: "Fitness club Center",
+  location: "central ave. 45, 5th floor",
+  classes: [
+    {
+      name: "yoga",
+      startsAt: "8:00 AM",
+      duration: 60,
+    },
+    {
+      name: "trx",
+      startsAt: "11:00 AM",
+      duration: 45,
+    },
+    {
+      name: "swimming",
+      startsAt: "3:00 PM",
+      duration: 70,
+    },
+  ],
+  futureClasses: [
+    {
+      name: "boxing",
+      willStartsAt: "6:00 PM",
+      duration: 40,
+    },
+    {
+      name: "breath training",
+      willStartsAt: "8:00 PM",
+      duration: 30,
+    },
+  ],
+  currClients: [
+    {
+      name: "John Smith",
+      age: "-",
+      gender: "male",
+      timeLeft: "1 month",
+    },
+    {
+      name: "Alise Smith",
+      age: 35,
+      gender: "female",
+      timeLeft: "3 month",
+    },
+    {
+      name: "Ann Sonne",
+      age: 24,
+      gender: "female",
+      timeLeft: "5 month",
+    },
+  ],
+  exClients: [
+    {
+      name: "Tom Smooth",
+      age: 50,
+      gender: "male",
+      makeCallFor: new Date("2023-08-12"),
+    },
+  ],
+  futureClients: [
+    {
+      name: "Maria",
+      makeCallFor: new Date("2023-07-10"),
+    },
+  ],
 };

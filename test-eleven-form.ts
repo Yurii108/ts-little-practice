85119d68207b8e680c73d099e2195c7988dae747
Yurii <paraska108@gmail.com>
2023-02-01 13:10:02 +0200

    finished all

--- test-eleven-form.ts
+++ test-eleven-form.ts
@@ -1,13 +1,19 @@
 interface IForm {
-	login: string;
-	password: string;
+  login: ILogin | string;
+  password: Password;
 }
 
+interface ILogin {
+  isValid: boolean;
+  errorMsg: string;
+}
+
+type Password = Omit<ILogin, "errorMsg">;
 // Необходимо типизировать объект валидации
 // Учтите, что данные в форме могут расширяться и эти поля
 // должны появиться и в объекте валидации
 
-const validationData = {
-	login: { isValid: false, errorMsg: "At least 3 characters" },
-	password: { isValid: true },
+const validationData: IForm = {
+  login: { isValid: false, errorMsg: "At least 3 characters" },
+  password: { isValid: true },
 };
--- test-ten-slider.ts
+++ test-ten-slider.ts
@@ -1,22 +1,22 @@
 interface ISlider {
-	container?: string;
-	numberOfSlides?: number;
-	speed?: 300 | 500 | 700;
-	direction?: "horizontal" | "vertical";
-	dots?: boolean;
-	arrows?: boolean;
-	animationName?: string;
+  container?: string;
+  numberOfSlides?: number;
+  speed?: 300 | 500 | 700;
+  direction?: "horizontal" | "vertical";
+  dots?: boolean;
+  arrows?: boolean;
+  animationName?: string;
 }
 
 function createSlider({
-	container = "",
-	numberOfSlides = 1,
-	speed = 300,
-	direction = "horizontal",
-	dots = true,
-	arrows = true,
+  container = "",
+  numberOfSlides = 1,
+  speed = 300,
+  direction = "horizontal",
+  dots = true,
+  arrows = true,
 }: ISlider = {}): void {
-	console.log(container, numberOfSlides, speed, direction, dots, arrows);
+  console.log(container, numberOfSlides, speed, direction, dots, arrows);
 }
 
 createSlider();
@@ -24,17 +24,35 @@ createSlider();
 // Необходимо типизировать объект настроек, который будет зависим
 // от интерфейса ISlider
 // Все поля в нем обязательны для заполнения
-const customSliderOptions = {
-	container: "id",
-	numberOfSlides: 4,
-	speed: 1100,
-	direction: "horizontal",
-	dots: true,
-	arrows: true,
+
+type SliderWithoutAnimName = Omit<ISlider, "animationName" | "speed">;
+
+interface ICustomSliderAddSpeed extends SliderWithoutAnimName {
+  speed: number;
+}
+
+type CopyCustomSlider<T> = {
+  [P in keyof T]-?: T[P];
 };
 
-function createCustomSlider(options: any): void {
-	if ("container" in options) {
-		console.log(options);
-	}
+type CreateCustomSlider = CopyCustomSlider<ICustomSliderAddSpeed>;
+
+const customSliderOptions: CreateCustomSlider = {
+  container: "id",
+  numberOfSlides: 4,
+  speed: 1100,
+  direction: "horizontal",
+  dots: true,
+  arrows: true,
+};
+
+function createCustomSlider(
+  options: CreateCustomSlider
+): CreateCustomSlider | string {
+  if ("container" in options) {
+    console.log(options);
+    return options;
+  } else {
+    return "Props in this obj don't have";
+  }
 }
